38c6e2b768350d73e0da203a0c54598cfccdf757
Chandima Ranaweera <chan4lk@gmail.com>
2021-12-16 14:19:27 +0530

    initial commit

---
+++ src/Calculator.js
@@ -0,0 +1,15 @@
+class Bill {
+    constructor() {
+    }
+    /**
+     * Calculates the bill.
+     * @param {number} units
+     * @param {1 | 2 | 3} type 1 = Normal, 2 = Commercial, 3 = Religious
+     * @param {number} previous
+     * @returns The bill value
+     */
+    calculate(units, type, previous) {
+       return null;
+    }
+}
+
